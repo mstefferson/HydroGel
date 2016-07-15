e9a6f6eb58ade1fffa87f8339653f456825c2cce
Michael Stefferson <mstefferson@gmail.com>
2016-07-15 17:20:41 -0600

    Big push on flux routines. Now have flux at SS, and slope/time at j = jss /2.

---
+++ FluxAtSS.m
@@ -0,0 +1,114 @@
+% Find the flux at steady state for various parameter configurations
+% Loops over KDinv, koff, nu
+
+save_me = 0;
+plotmap_flag = 1;
+
+% Looped parameters
+% Kon calculated in loop
+nuVec  = [0 1 10];
+% KDinvVec = [0 1e2 1e3 1e4 1e5 ];
+% KoffVec = [ 1e-1 1e0 1e1 1e2 1e3 ];
+% KDinvVec = [0 logspace(3,4.5,12) ];
+% KoffVec = [ logspace(2,4,12) ];
+KDinvVec = [0 logspace(3,5.5,40) ];
+KoffVec = [ logspace(1,5.5,40) ];
+savestr_fa = ['flxss'];
+
+% Non-loopable parameters
+linearEqn = 0;
+BCstr = 'DirVn'; % 'Dir','Vn','DirVn'
+DA  = 1;
+AL  = 2e-4;
+AR  = 0;
+Bt  = 2e-3;
+Nx  = 1000;
+Lbox = 1;
+
+% Flux matrix
+fluxSS = zeros( length( nuVec ), length(KDinvVec), length( KoffVec ) );
+
+% Calculated things
+x = linspace(0, Lbox, Nx) ;
+dx  = x(2) - x(1);
+
+% Run the loops
+for i = 1:length(nuVec)
+  nu = nuVec(i);
+  if nu == 0
+    x = linspace(0, Lbox, Nx) ;
+    dx = x(2) - x(1);
+    AnlOde = (AR - AL) * x / Lbox + AL;
+    CnlOde = zeros(1,Nx);
+    flux   = - DA * ( AnlOde(end) - AnlOde(end-1) ) / dx;
+    fluxSS( i, :, : ) = flux; 
+  else
+    for j = 1:length(KDinvVec)
+      KDinv = KDinvVec(j);
+      if KDinv == 0
+        x = linspace(0, Lbox, Nx) ;
+        dx = x(2) - x(1);
+        AnlOde = (AR - AL) * x / Lbox + AL;
+        CnlOde = zeros(1,Nx);
+        flux   = - DA * ( AnlOde(end) - AnlOde(end-1) ) / dx;
+        fluxSS( i, j, : ) = flux;
+      else
+        parfor k = 1:length(KoffVec)
+          Koff = KoffVec(k);
+          Kon  = Koff * KDinv;
+          
+          [AnlOde,CnlOde,x] = RdSsSolverMatBvFunc(...
+            Kon,Koff,nu,AL,AR,Bt,Lbox,BCstr,Nx,linearEqn);
+          dx = x(2) - x(1);
+          flux   = - DA * ( AnlOde(end) - AnlOde(end-1) ) / dx;
+          fluxSS( i, j, k ) = flux;
+        end % loop Koff
+      end % Kdinv = 0
+    end % loop Kdinv
+  end % if nu == 0
+end % loop nu
+
+
+
+%% Surface plot
+if plotmap_flag
+  
+  for i = 1:length(nuVec)
+    figure()
+    
+    % Flux
+    imagesc( 1:length(KoffVec), 1:length(KDinvVec), ...
+      reshape( fluxSS(i,:,:), [length(KDinvVec) length(KoffVec) ] ) );
+    xlabel( 'Koff'); ylabel('KdInv');
+    Ax = gca;
+    Ax.YTick = 1:4:length(KDinvVec);
+    Ax.YTickLabel = num2cell( round( KDinvVec(1:4:end) ) );
+    Ax.XTick = 1: 4: length(KoffVec) ;
+    Ax.XTickLabel = num2cell( round (KoffVec (1:4:end) ) );
+    titstr = sprintf( 'Max Flux nu = %g', nuVec(i) );
+    title(titstr)
+    colorbar
+    axis square
+    
+    % Save stuff
+    if save_me
+      savefig( gcf, [savestr_fa  '_nu'...
+        num2str( nuVec(i) ) '.fig'] );
+      saveas( gcf, [ savestr_fa '_nu'...
+        num2str( nuVec(i) ) ], 'jpg' );
+    end
+  end
+  
+  if save_me
+    save('FluxAtSS.mat', 'fluxSS', 'nuVec', 'KDinvVec', 'KoffVec');
+  end
+    
+end
+
+
+
+
+
+
+
+
--- LoopParamsFlux.m
+++ LoopParamsFlux.m
@@ -1,8 +1,17 @@
 % Build vectors and matrices
-KDinvVec = [0 1e1 1e4];
-KoffVec = [1e-2 1e-1 1e0 1e1];
-FluxVsT = zeros( length(KDinvVec) , length(KoffVec), TimeObj.N_rec );
-AccumVsT = zeros( length(KDinvVec) , length(KoffVec), TimeObj.N_rec );
+%  KDinvVec = [0 logspace(3,4.5,12) ];
+%  KoffVec = [ logspace(2,4,12) ];
+
+% KDinvVec = [0 linspace(1e4 - 500,1e4 + 500,12) ];
+% KoffVec = [ logspace(1e2 - 50, 1e2 + 50,12) ];
+nu       = 1;
+KDinvVec = [0 1e2 1e3 1e4 1e5 ];
+KoffVec = [ 1e-1 1e0 1e1 1e2 1e3 ];
+dt_fac   = 0.5;
+savestr_vt = 'flxvst';
+savestr_fa = ['flxaccummap'];
+plotvst_flag = 0;
+plotmap_flag = 1;
 
 % Add paths and see where we are
 addpath('./Subroutines');
@@ -11,7 +20,7 @@ Time = datestr(now);
 currentdir=pwd;
 fprintf('In dir %s\n',currentdir);
 
-fprintf('Starting RunHydroGel: %s\n', Time)
+fprintf('Starting Flux Loop: %s\n', Time)
 
 % Initparams
 fprintf('Initiating parameters\n');
@@ -22,14 +31,17 @@ else
   InitParams
 end
 
+ParamObj.Dc    = nu; % Dc/Da
+
 % "Analysis" subroutines
 AnalysisObj.QuickMovie=0; AnalysisObj.TrackAccumFromFlux= 1;
-AnalysisObj.TrackAccumFromFluxPlot=0; AnalysisObj.PlotMeLastConc=0; 
-AnalysisObj.PlotMeAccum=0; AnalysisObj.PlotMeWaveFrontAccum=0;  
+AnalysisObj.TrackAccumFromFluxPlot=0; AnalysisObj.PlotMeLastConc=0;
+AnalysisObj.PlotMeAccum=0; AnalysisObj.PlotMeWaveFrontAccum=0;
 AnalysisObj.PlotMeLastConcAccum=0; AnalysisObj.CheckConservDen=0;
-AnalysisObj.ShowRunTime=1;  
+AnalysisObj.ShowRunTime=1;
 
 % Build TimeObj
+dt = dt * dt_fac;
 [TimeObj] = TimeObjMakerRD(dt,t_tot,t_rec,ss_epsilon,NumPlots);
 
 FileDir = sprintf('RdNx%dA%sC%st%d',...
@@ -47,6 +59,9 @@ fprintf('trial:%d A_BC: %s C_BC: %s\n', ...
 disp(ParamObj); disp(AnalysisObj); disp(TimeObj);
 
 % Edits here. Change params and loop over
+FluxVsT = zeros( length(KDinvVec) , length(KoffVec), TimeObj.N_rec );
+AccumVsT = zeros( length(KDinvVec) , length(KoffVec), TimeObj.N_rec );
+
 global Flux2ResR_rec;
 global FluxAccum_rec;
 
@@ -66,66 +81,108 @@ for ii = 1:length(KDinvVec)
       ParamObj.Kon   = ParamObj.KDinv * ParamObj.Koff;
       fprintf( 'Koff = %f Kon = %f\n',ParamObj.Koff,ParamObj.Kon );
       [A,C,DidIBreak,SteadyState] = ChemDiffMain(ParamObj,TimeObj,AnalysisObj);
-      FluxVsT(ii,jj,:) = Flux2ResR_rec;
-      AccumVsT(ii,jj,:) = FluxAccum_rec;
       fprintf('Break = %d Steady = %d\n',DidIBreak,SteadyState)
+      FluxVsT(ii,jj,1:length( Flux2ResR_rec )) = Flux2ResR_rec;
+      AccumVsT(ii,jj,1:length( Flux2ResR_rec )) = FluxAccum_rec;
+      if SteadyState
+        FluxVsT(ii,jj,length( Flux2ResR_rec ) + 1:TimeObj.N_rec) = ...
+          Flux2ResR_rec(end);
+        AccumVsT(ii,jj,length( Flux2ResR_rec ) + 1:TimeObj.N_rec) = ...
+          FluxAccum_rec(end) + Flux2ResR_rec(end) * ...
+          TimeObj.t_rec .* (1: TimeObj.N_rec -length( Flux2ResR_rec )  ) ;
+      end
     end
   end
 end
-      
+
 FluxMax = FluxVsT(:,:,end);
 AccumMax = AccumVsT(:,:,end);
 
-% Plot time
-TimeVec = (0:TimeObj.N_rec-1) * t_rec;
-[Tr, Tc] =  size(TimeVec);
+%% Plot time
 
-% Plot Koffs vs time for each KdInv
-for ii = 2:length(KDinvVec)
-  figure()
-  hold all
+% Plot vs time
+if plotvst_flag
+  TimeVec = (0:TimeObj.N_rec-1) * t_rec;
+  [Tr, Tc] =  size(TimeVec);
 
-  for jj = 1:length(KoffVec)
-    plotyy( TimeVec, reshape( FluxVsT(ii,jj,:), [Tr Tc] ),...
-      TimeVec, reshape( AccumVsT(ii,jj,:), [Tr Tc] ) );
+  % Plot Koffs vs time for each KdInv
+  for ii = 2:length(KDinvVec)
+    figure()
+    % Plot it
+    AH1 = subplot(1,2,1);
+    axis square
+    hold all
+    AH2 = subplot(1,2,2);
+    axis square
+    hold all
+    for jj = 1:length(KoffVec)
+      plot( AH1, TimeVec, reshape( FluxVsT(ii,jj,:), [Tr Tc] ) );
+      plot( AH2, TimeVec, reshape( AccumVsT(ii,jj,:), [Tr Tc] ) );
+    end
+    plot( AH1, TimeVec, reshape( FluxVsT(1,1,:), [Tr Tc] ) );
+    plot( AH2, TimeVec, reshape( AccumVsT(1,1,:), [Tr Tc] ) );
+    %Axis
+    xlabel(AH1,'time'); xlabel(AH2,'time');
+    ylabel(AH1,'flux'); ylabel(AH2,'accumultation');
+    %   AH1.YLim = [ 0 1e-3 ]; AH2.YLim = [ 0 5e-4 ];
+    % Titles
+    titstr = sprintf('Kd^{-1} = %g (BA)', KDinvVec(ii) );
+    title(AH1,titstr);
+    titstr = sprintf('Dc/Da = %g', ParamObj.Dc );
+    title(AH2,titstr);
+    % Legend stuff
+    legcell = cell( length(KoffVec) + 1, 1 );
+    for i = 1:length(KoffVec)
+      legcell{i} = ['Koff = ' num2str( KoffVec(i) ) ];
+    end
+    legcell{end} = 'No binding';
+    h = legend(AH2,legcell,'location','best');
+    h.Position(1:2) = [0.525 0.35];
+    % Save stuff
+    savestr_vts = [savestr_vt '_Kdinv' num2str(round(KDinvVec(ii)))...
+      '_nu' num2str(ParamObj.Dc/ParamObj.Da) ];
+    savefig( gcf, [savestr_vts '.fig'] );
+    saveas( gcf, savestr_vts, 'jpg' );
   end
-  plotyy( TimeVec, reshape( FluxVsT(1,1,:), [Tr Tc] ),...
-    TimeVec, reshape( AccumVsT(1,1,:), [Tr Tc] ) );
-  xlabel('time'); ylabel('flux');
-  titstr = sprintf('Kd^{-1} = %g (Binding Affinity)', KDinvVec(ii) );
-  title(titstr);
-  Ax = gca;
-  Ax.YLim = [ 0 1e-3 ];
-
-  legcell = cell( length(KoffVec) + 1, 1 );
-  for i = 1:length(KoffVec)
-   legcell{i} = ['Koff = ' num2str( KoffVec(i) ) ];
-  end
-  legcell{end} = 'No binding';
-  legend(legcell,'location','best');
-
 end
 
-% Surface plot
-figure()
-imagesc( 1:length(KoffVec), 1:length(KDinvVec),  FluxMax);
-xlabel( 'Koff'); ylabel('KdInv');
-Ax = gca;
-Ax.YTick = 1:length(KDinvVec);
-Ax.YTickLabel = num2cell( KDinvVec );
-Ax.XTick = 1:length(KoffVec);
-Ax.XTickLabel = num2cell( KoffVec );
-title('Max Flux')
-colorbar
 
-% Surface plot
-figure()
-imagesc( 1:length(KoffVec), 1:length(KDinvVec),  AccumMax);
-xlabel( 'Koff'); ylabel('KdInv');
-Ax = gca;
-Ax.YTick = 1:length(KDinvVec);
-Ax.YTickLabel = num2cell( KDinvVec );
-Ax.XTick = 1:length(KoffVec);
-Ax.XTickLabel = num2cell( KoffVec );
-title('Max Accum')
-colorbar
+%% Surface plot
+if plotmap_flag
+  figure()
+
+  % Flux
+  subplot(1,2,1);
+  imagesc( 1:length(KoffVec), 1:length(KDinvVec),  FluxMax);
+  xlabel( 'Koff'); ylabel('KdInv');
+  Ax = gca;
+  Ax.YTick = 1:length(KDinvVec);
+  Ax.YTickLabel = num2cell( KDinvVec );
+  Ax.XTick = 1: 2: length(KoffVec) ;
+  Ax.XTickLabel = num2cell( round (KoffVec (1:2:end) ) );
+  titstr = sprintf('Max Flux nu = %g',ParamObj.Dc);
+  title(titstr)
+  colorbar
+  axis square
+
+  % Accumulation
+  subplot(1,2,2);
+  imagesc( 1:length(KoffVec), 1:length(KDinvVec),  AccumMax);
+  xlabel( 'Koff'); ylabel('KdInv');
+  Ax = gca;
+  Ax.YTick = 1:length(KDinvVec);
+  Ax.YTickLabel = num2cell( KDinvVec );
+  Ax.XTick = 1: 2: length(KoffVec) ;
+  Ax.XTickLabel = num2cell( round (KoffVec (1:2:end) ) );
+  titstr = sprintf('Max Accum nu = %g',ParamObj.Dc);
+  title(titstr)
+  colorbar
+  axis square
+
+  %%
+  % Save stuff
+  savefig( gcf, [savestr_fa  '_nu'...
+    num2str(ParamObj.Dc/ParamObj.Da) '.fig'] );
+  saveas( gcf, [ savestr_fa '_nu'...
+    num2str(ParamObj.Dc/ParamObj.Da) ], 'jpg' );
+end
