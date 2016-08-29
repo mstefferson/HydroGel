64e5732797cbdaf519ffbbe16a960dbe9df5b881
Michael Stefferson <mstefferson@gmail.com>
2016-08-29 17:21:10 -0600

    Removed extra ChemDiffMain* and now LoopParamsFlux calls ChemDiffMain. Also, added a cleanme

--- LoopParamsFlux.m
+++ LoopParamsFlux.m
@@ -1,18 +1,26 @@
-% Build vectors and matrices
-%  KDinvVec = [0 logspace(3,4.5,12) ];
-%  KoffVec = [ logspace(2,4,12) ];
+% LoopParamsFlux
+% 
+% Loops over parameter Kon*Bt (Bt fixed) and KoffVec 
+% and calculates the max flux, slope, and time to half max
 
-% KDinvVec = [0 linspace(1e4 - 500,1e4 + 500,12) ];
-% KoffVec = [ logspace(1e2 - 50, 1e2 + 50,12) ];
-saveMe     = 1;
-nuVec       = [0 1 10];
-KDinvVec = [0 logspace(3,5,12) ];
-KoffVec = [ logspace(1,4,16)  ];
+saveMe     = 0;
+nuVec       = [1];
+KonBtVec    = [  logspace(1,5,5) ];
+KoffVec     = [  logspace(1,5,5)  ];
 dt_fac   = 0.5;
 savestr_vt = 'flxvst';
 savestr_fm = ['flxmapSs'];
+savematstr = 'fluxLin.mat';
 plotvst_flag = 1;
-plotmap_flag = 1;
+plotmap_max_flag = 1;
+plotmap_slope_flag = 1;
+plotmap_time_flag = 1;
+
+if plotmap_max_flag == 1 || plotmap_max_flag == 1 || plotmap_max_flag == 1;
+  plotmap_flag = 1;
+else
+  plotmap_max_flag = 0;
+end
 
 % Add paths and see where we are
 addpath('./Subroutines');
@@ -51,9 +59,11 @@ fprintf('trial:%d A_BC: %s C_BC: %s\n', ...
 disp(ParamObj); disp(AnalysisObj); disp(TimeObj);
 
 % Edits here. Change params and loop over
-FluxVsT = zeros( length(nuVec), length(KDinvVec) , length(KoffVec), TimeObj.N_rec );
-AccumVsT = zeros( length(nuVec), length(KDinvVec) , length(KoffVec), TimeObj.N_rec );
+FluxVsT = zeros( length(nuVec), length(KonBtVec) , length(KoffVec), TimeObj.N_rec );
+AccumVsT = zeros( length(nuVec), length(KonBtVec) , length(KoffVec), TimeObj.N_rec );
 
+FluxVsTDiff  = zeros( 1, TimeObj.N_rec );
+AccumVsTDiff = zeros( 1, TimeObj.N_rec );
 
 % Run Diff first
 Koff = 0;
@@ -65,24 +75,17 @@ dt = dtSave;
 % keyboard
 % temp = repmat( reshape( Flux2ResR_rec, [1 1 1 length(Flux2ResR_rec) ] ) , ...
 %   [1 length(nuVec) length(KoffVec) 1] );
-FluxVsT(:,1,:,:) = repmat( reshape( Flux2ResR_rec, [1 1 1 length(Flux2ResR_rec) ] ) , ...
-  [1 length(nuVec) length(KoffVec) 1] );
-AccumVsT(:,1,:,:) = repmat( reshape( FluxAccum_rec, [1 1 1 length(Flux2ResR_rec) ] ) , ...
-  [1 length(nuVec) length(KoffVec) 1] );
+FluxVsTDiff = Flux2ResR_rec;
+AccumVsTDiff = FluxAccum_rec;
 
 for ii = 1:length(nuVec)
   ParamObj.Dc  = nuVec(ii);
   fprintf('\n\n Starting nu = %g \n\n', ParamObj.Dc );
-  for jj = 2:length(KDinvVec)
-    ParamObj.KDinv = KDinvVec(jj);
-    KDinv = ParamObj.KDinv;
-    fprintf('\n\n Starting Kd = %f \n\n', ParamObj.KDinv );
+  for jj = 1:length(KonBtVec)
+    Kon = KonBtVec(jj) / ParamObj.Bt;
+    fprintf('\n\n Starting Kon Bt = %f \n\n', KonBtVec(jj) );
     parfor kk = 1:length(KoffVec)
-      %       ParamObj.Koff = KoffVec(kk);
-      %       ParamObj.Kon   = ParamObj.KDinv * ParamObj.Koff;
-      
       Koff = KoffVec(kk);
-      Kon  = KDinv * Koff;
       
       if Kon > 1e8
         dt = dtSave / 10;
@@ -110,12 +113,12 @@ TimeVec = (0:TimeObj.N_rec-1) * t_rec;
 jMax = FluxVsT(:,:,:,end);
 aMax = AccumVsT(:,:,:,end);
 
-djdtHm = zeros( length(nuVec), length(KDinvVec) , length(KoffVec)  );
-tHm = zeros( length(nuVec), length(KDinvVec) , length(KoffVec)  );
+djdtHm = zeros( length(nuVec), length(KonBtVec ) , length(KoffVec)  );
+tHm = zeros( length(nuVec), length(KonBtVec ) , length(KoffVec)  );
 
 
 for ii = 1:length(nuVec)
-  for jj = 1:length(KDinvVec)
+  for jj = 1:length(KonBtVec )
     for kk = 1:length(KoffVec)
       
       % Find index where flux passes half max
@@ -145,7 +148,7 @@ if plotvst_flag
   legcell{end} = 'No binding';
   for ii = 1:length(nuVec)
     
-    for jj = 2:length(KDinvVec)
+    for jj = 1:length(KonBtVec)
       figure();
       % Plot it
       AH1 = subplot(1,2,1);
@@ -159,21 +162,21 @@ if plotvst_flag
         plot( AH2, TimeVec, reshape( AccumVsT(ii,jj,kk,:), [Tr Tc] ) );
         legcell{kk} = ['Koff = ' num2str( KoffVec(kk) ) ];
       end
-      plot( AH1, TimeVec, reshape( FluxVsT(ii,1,1,:), [Tr Tc] ) );
-      plot( AH2, TimeVec, reshape( AccumVsT(ii,1,1,:), [Tr Tc] ) );
+      plot( AH1, TimeVec, FluxVsTDiff);
+      plot( AH2, TimeVec, AccumVsTDiff);
       %Axis
       xlabel(AH1,'time'); xlabel(AH2,'time');
       ylabel(AH1,'flux'); ylabel(AH2,'accumultation');
       %   AH1.YLim = [ 0 1e-3 ]; AH2.YLim = [ 0 5e-4 ];
       % Titles
-      titstr = sprintf('Kd^{-1} = %g (BA)', KDinvVec(jj) );
+      titstr = sprintf('Kon * Bt = %g (BA)', KonBtVec(jj) );
       title(AH1,titstr);
       titstr = sprintf('Dc/Da = %g', nuVec(ii) );
       title(AH2,titstr);
       h = legend(AH2,legcell,'location','best');
       h.Position(1:2) = [0.525 0.35];
       % Save stuff
-      savestr_vts = [savestr_vt '_Kdinv' num2str(round(KDinvVec(jj)))...
+      savestr_vts = [savestr_vt '_Kdinv' num2str(round(KonBtVec(jj)))...
         '_nu' num2str(nuVec(ii)) ];
       savefig( gcf, [savestr_vts '.fig'] );
       saveas( gcf, savestr_vts, 'jpg' );
@@ -186,76 +189,88 @@ end
 if plotmap_flag
   
   deltaTick = 2;
+  xstr = ' K_{off} / \tau ';
+  ystr = ' K_{on}B_{t} / \tau ';
   for ii = 1:length(nuVec)
-    figure()
+    
     
     % Flux Max
-    imagesc( 1:length(KoffVec), 1:length(KDinvVec), ...
-      reshape( jMax(ii,:,:), [length(KDinvVec) length(KoffVec) ] ) );
-    xlabel( 'Koff'); ylabel('KdInv');
-    Ax = gca;
-    Ax.YTick = 1:deltaTick:length(KDinvVec);
-    Ax.YTickLabel = num2cell( round( KDinvVec(1:deltaTick:end) ) );
-    Ax.XTick = 1:deltaTick: length(KoffVec) ;
-    Ax.XTickLabel = num2cell( round (KoffVec (1:deltaTick:end) ) );
-    titstr = sprintf( 'Max Flux nu = %g', nuVec(ii) );
-    title(titstr)
-    colorbar
-    axis square
-    if saveMe   ;
-      savefig( gcf, [savestr_fm '_max' '_nu'...
-        num2str( nuVec(ii) ) '.fig'] );
-      saveas( gcf, [ savestr_fm '_max' '_nu'...
-        num2str( nuVec(ii) ) ], 'jpg' );
-    end
-         
-    figure()
-    % Slope at half max
-    imagesc( 1:length(KoffVec), 1:length(KDinvVec), ...
-      reshape( djdtHm(ii,:,:), [length(KDinvVec) length(KoffVec) ] ) );
-    xlabel( 'Koff'); ylabel('KdInv');
-    Ax = gca;
-    Ax.YTick = 1:deltaTick:length(KDinvVec);
-    Ax.YTickLabel = num2cell( round( KDinvVec(1:deltaTick:end) ) );
-    Ax.XTick = 1:deltaTick: length(KoffVec) ;
-    Ax.XTickLabel = num2cell( round (KoffVec (1:deltaTick:end) ) );
-    titstr = sprintf( 'Slope, dj/dt, at Half Max Flux nu = %g', nuVec(ii) );
-    title(titstr)
-    colorbar
-    axis square
-    if saveMe   ;
-      savefig( gcf, [savestr_fm '_slopeHm'  '_nu'...
-        num2str( nuVec(ii) ) '.fig'] );
-      saveas( gcf, [ savestr_fm '_slopeHm' '_nu'...
-        num2str( nuVec(ii) ) ], 'jpg' );
+    if plotmap_max_flag == 1
+      figure()
+      imagesc( 1:length(KoffVec), 1:length(KonBtVec), ...
+        reshape( jMax(ii,:,:), [length(KonBtVec) length(KoffVec) ] ) );
+      xlabel( xstr ); ylabel( ystr );
+      Ax = gca;
+      Ax.YTick = 1:deltaTick:length(KonBtVec);
+      Ax.YTickLabel = num2cell( round( KonBtVec(1:deltaTick:end) ) );
+      Ax.XTick = 1:deltaTick: length(KoffVec) ;
+      Ax.XTickLabel = num2cell( round (KoffVec (1:deltaTick:end) ) );
+      Ax.YDir = 'normal';
+      titstr = sprintf( 'Max Flux nu = %g', nuVec(ii) );
+      title(titstr)
+      colorbar
+      axis square
+      if saveMe   ;
+        savefig( gcf, [savestr_fm '_max' '_nu'...
+          num2str( nuVec(ii) ) '.fig'] );
+        saveas( gcf, [ savestr_fm '_max' '_nu'...
+          num2str( nuVec(ii) ) ], 'jpg' );
+      end
     end
     
-    figure()
-    % Time at half max
-    imagesc( 1:length(KoffVec), 1:length(KDinvVec), ...
-      reshape( tHm(ii,:,:), [length(KDinvVec) length(KoffVec) ] ) );
-    xlabel( 'Koff'); ylabel('KdInv');
-    Ax = gca;
-    Ax.YTick = 1:deltaTick:length(KDinvVec);
-    Ax.YTickLabel = num2cell( round( KDinvVec(1:deltaTick:end) ) );
-    Ax.XTick = 1:deltaTick: length(KoffVec) ;
-    Ax.XTickLabel = num2cell( round (KoffVec (1:deltaTick:end) ) );
-    titstr = sprintf( ' Time at Half Max Flux nu = %g', nuVec(ii) );
-    title(titstr)
-    colorbar
-    axis square
-    if saveMe   ;
-      savefig( gcf, [savestr_fm  '_tHm' '_nu'...
-        num2str( nuVec(ii) ) '.fig'] );
-      saveas( gcf, [ savestr_fm '_tHm' '_nu'...
-        num2str( nuVec(ii) ) ], 'jpg' );
+    if plotmap_slope_flag == 1
+      figure()
+      % Slope at half max
+      imagesc( 1:length(KoffVec), 1:length(KonBtVec), ...
+        reshape( djdtHm(ii,:,:), [length(KonBtVec) length(KoffVec) ] ) );
+      xlabel( xstr ); ylabel(  ystr );
+      Ax = gca;
+      Ax.YTick = 1:deltaTick:length(KonBtVec);
+      Ax.YTickLabel = num2cell( round( KonBtVec(1:deltaTick:end) ) );
+      Ax.XTick = 1:deltaTick: length(KoffVec) ;
+      Ax.XTickLabel = num2cell( round (KoffVec (1:deltaTick:end) ) );
+      Ax.YDir = 'normal';
+      titstr = sprintf( 'Slope, dj/dt, at Half Max Flux nu = %g', nuVec(ii) );
+      title(titstr)
+      colorbar
+      axis square
+      if saveMe   ;
+        savefig( gcf, [savestr_fm '_slopeHm'  '_nu'...
+          num2str( nuVec(ii) ) '.fig'] );
+        saveas( gcf, [ savestr_fm '_slopeHm' '_nu'...
+          num2str( nuVec(ii) ) ], 'jpg' );
+      end
+    end
+    
+    if plotmap_time_flag == 1
+      figure()
+      % Time at half max
+      imagesc( 1:length(KoffVec), 1:length(KonBtVec), ...
+        reshape( tHm(ii,:,:), [length(KonBtVec) length(KoffVec) ] ) );
+      xlabel( xstr ); ylabel( ystr );
+      Ax = gca;
+      Ax.YTick = 1:deltaTick:length(KonBtVec);
+      Ax.YTickLabel = num2cell( round( KonBtVec(1:deltaTick:end) ) );
+      Ax.XTick = 1:deltaTick: length(KoffVec) ;
+      Ax.XTickLabel = num2cell( round (KoffVec (1:deltaTick:end) ) );
+      Ax.YDir = 'normal';
+      titstr = sprintf( ' Time at Half Max Flux nu = %g', nuVec(ii) );
+      title(titstr)
+      colorbar
+      axis square
+      if saveMe   ;
+        savefig( gcf, [savestr_fm  '_tHm' '_nu'...
+          num2str( nuVec(ii) ) '.fig'] );
+        saveas( gcf, [ savestr_fm '_tHm' '_nu'...
+          num2str( nuVec(ii) ) ], 'jpg' );
+      end
     end
   end % loop nu
 end % plot flag
 
 %%
 if saveMe
-  save('fluxStuff.mat', 'FluxVsT', 'jMax', ...
-    'djdtHm','tHm', 'nuVec','KDinvVec','KoffVec','TimeVec');
+  save(savematstr, 'FluxVsT', 'jMax', ...
+    'djdtHm','tHm', 'nuVec','KonBtVec','KoffVec','TimeVec');
 end
-  
+
