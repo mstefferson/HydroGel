746734c94098ec04a585979b80c8c9212ec4aae9
Michael Stefferson <mstefferson@gmail.com>
2017-08-22 16:48:18 -0600

    nonDimParam can now scale by diff, k_on, k_off

--- nonDimParamCalc.m
+++ nonDimParamCalc.m
@@ -6,7 +6,7 @@
 %
 % All units should be in SI units (seconds, meters, Molar)
 %
-function [ scaledParams, physParams ] = ...
+function [ scaledDiff, scaledKon, scaledKoff, physParams ] = ...
   nonDimParamCalc( lBox, dA, kD, kon, bt, lplc )
 if nargin ~= 6
   error('Incorrect number of inputs')
@@ -15,14 +15,6 @@ end
 koff = kon * kD;
 konBt = kon * bt;
 dC = dA * lplc * koff / ( 3  * dA + lplc * koff );
-% scaled
-tau = lBox ^ 2 ./ dA;
-kappaOff = koff * tau;
-kappaOnBt = konBt * tau;
-kappaOn = kon * tau;
-lPlC = lplc ./ (lBox .^ 2);
-nu = kappaOff * lPlC ./ ( 3 + kappaOff .* lPlC );
-% store it
 % phys
 physParams.lBox = lBox;
 physParams.dA = dA;
@@ -32,13 +24,58 @@ physParams.konBt = konBt;
 physParams.koff = koff;
 physParams.bt = bt;
 physParams.lplc = lplc;
-% scaled
-scaledParams.Lbox = 1;
-scaledParams.Da = 1;
-scaledParams.tau = tau;
-scaledParams.nu = nu;
-scaledParams.Kon = kappaOn;
-scaledParams.KonBt = kappaOnBt;
-scaledParams.Koff = kappaOff ;
-scaledParams.Bt = bt;
-scaledParams.Llp = lPlC;
+% scaled diffusion
+tau = lBox ^ 2 ./ dA;
+L = lBox;
+kappaOff = koff * tau;
+kappaOnBt = konBt * tau;
+kappaOn = kappaOnBt / bt;
+lPlC = lplc ./ (L .^ 2);
+nu =  lPlC ./ ( 3 * dA / (L^2 * koff) + lPlC );
+% store it
+scaledDiff.tau = tau;
+scaledDiff.L = lBox;
+scaledDiff.Lbox = 1;
+scaledDiff.Da = 1;
+scaledDiff.nu = nu;
+scaledDiff.Kon = kappaOn;
+scaledDiff.KonBt = kappaOnBt;
+scaledDiff.Koff = kappaOff ;
+scaledDiff.Bt = bt;
+scaledDiff.Llp = lPlC;
+% scaled kon
+tau = 1 / (kon * bt) ;
+L = sqrt( dA ./ (konBt) );
+kappaOff = kD / bt;
+kappaOnBt = 1;
+kappaOn = kappaOnBt / bt;
+lPlC = lplc ./ (L .^ 2);
+nu =  lPlC ./ ( 3 * dA / (L^2 * koff) + lPlC );
+% store it
+scaledKon.Lbox = lBox / L;
+scaledKon.Da = 1;
+scaledKon.tau = tau;
+scaledKon.nu = nu;
+scaledKon.Kon = kappaOn;
+scaledKon.KonBt = kappaOnBt;
+scaledKon.Koff = kappaOff ;
+scaledKon.Bt = bt;
+scaledKon.Llp = lPlC;
+% scale koff
+tau = 1 / (koff) ;
+L = sqrt( dA ./ (koff) );
+kappaOff = 1;
+kappaOnBt = bt / kD ;
+kappaOn =  kappaOnBt / bt;
+lPlC = lplc ./ (L .^ 2);
+nu =  lPlC ./ ( 3 * dA / (L^2 * koff) + lPlC );
+% store it
+scaledKoff.Lbox = lBox / L;
+scaledKoff.Da = 1;
+scaledKoff.tau = tau;
+scaledKoff.nu = nu;
+scaledKoff.Kon = kappaOn;
+scaledKoff.KonBt = kappaOnBt;
+scaledKoff.Koff = kappaOff ;
+scaledKoff.Bt = bt;
+scaledKoff.Llp = lPlC;
