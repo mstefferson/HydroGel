1bbf82270ebd3140266472b33ec9c2a0442d0da2
Michael Stefferson <mstefferson@gmail.com>
2018-01-08 13:02:36 -0700

    updated forgotten initParamsSFromInput

--- paperInits/initParamsSFromInput.m
+++ paperInits/initParamsSFromInput.m
@@ -23,38 +23,39 @@ analysisFlags.TrackProgress          = 1;  % Track run progress
 
 %Spatial grid
 paramMaster.Lbox  = 1; % Gel length
-Nx    = 100 * 1280;  % Internal gridpoints
-%Nx    = 1280;  % Internal gridpoints
-paramMaster.Nx    = floor(Nx*paramMaster.Lbox); % Scale by box. Careful!!!
+paramMaster.Nx = 12800; % number of grid points
 paramMaster.Lr = 10; % Reservoir length if there is one
-
-%Non Dimensional and Concentration. Code will only vary 2/3 of kinetic parameters
-% konBt, koff, Ka
-paramMaster.Da     = 1; % Diffusion of species A (unbound). Sets time scale
-paramMaster.DbParam     = {'nu', [1]}; 
+% diffusion coefficient
+paramMaster.Da = 1; % Diffusion of species A (unbound). Sets time scale
+% bound diffusion, either {'nu',[]},{'lplc',[]}
+% nu: actual value, lplc: bound tethered model
+paramMaster.DbParam     = {'nu', 1}; 
+% concentrations
+paramMaster.AL = 1e-6;  % concentration of inlet
+paramMaster.AR = 0; % concentration of outlet
+paramMaster.Bt = [1e-3];  % vec molar (old: 1e-2) (new: 1e-3)
+% Varying two kinetic parameters. First cell, name (str). Second, vector of values
+% options: {'konBt',[...]}, {'koff',[...]}, {'kD',[...]}, {'kA',[...]}
+kon = [1e9]; % if you want to change just kon, and not konBt, do it here
+konBt = buildKonBt( paramMaster.Bt, kon );
 % Varying only 2 of konbt, koff, Ka. Leave third blank []. e.g.
-paramMaster.kinParam1 = {'konBt', [1e4]};  % vec konBt (time scale)
-paramMaster.kinParam2 = {'kA', []};  % vec konBt (time scale)
-paramMaster.Bt     = [1e-3];  % vec molar (old: 1e-2) (new: 1e-3)
-paramMaster.Dnl    = 1; % Dsat/DA. Dnl = 1: (constant D); Dnl > 1 : D([A])
-paramMaster.AL     = 1e-6;  % concentration of inlet
-paramMaster.AR     = 0; % concentration of outlet
+paramMaster.kinParam1 = {'konBt', [konBt]};  % vec konBt (time scale)
+paramMaster.kinParam2 = {'kA', []};
+paramMaster.Dnl = 1; % Dsat/DA. Dnl = 1: (constant D); Dnl > 1 : D([A])
 
 % time
-tfac        = 2; % run time factor in relation to box diffusion time
-dtfac       = 1; % dt factor in relation to VN stability condition
+tfac = 1; % run time factor in relation to box diffusion time
+dtfac = 1; % dt factor in relation to VN stability condition
 timeMaster.dt = dtfac * ( (paramMaster.Lbox/paramMaster.Nx)^2 / paramMaster.Da ); % time step
-timeMaster.t_tot   = tfac * paramMaster.Lbox^2 /  paramMaster.Da;  % total time
-timeMaster.t_rec   = timeMaster.t_tot / 100;  % time interval for recording dynamics
+timeMaster.t_tot = tfac * paramMaster.Lbox^2 /  paramMaster.Da;  % total time
+timeMaster.t_rec = timeMaster.t_tot / 100;  % time interval for recording dynamics
 timeMaster.ss_epsilon = 1e-6;  % steady state condition
 timeMaster.NumPlots = 10; % For the accumulation plot subroutine
 
 % koff vary
 % {'const'}  or {}
 % {'outletboundary', multVal}
-% koffVary = {'outletboundary', [0.1 1 10]};
-koffVary = {'outletboundary', [1]};
-
+koffVary = {};
 % Binding flag 0: constant. 1: Square blurr
 paramMaster.alpha  = 0.1;  % length scale (frac of box) where binding sites change
 % Turn on if diffusion depends on Bt. If Bt varies spatially,
@@ -75,3 +76,11 @@ end
 % 'Mx': Fixed Concenctration on left and no flux right
 paramMaster.A_BC = 'Dir';
 paramMaster.C_BC = 'Vn';
+
+function konBt = buildKonBt( kon, bt )
+if isempty(kon)
+  konBt = [];
+else
+  konBt = reshape( bt' * kon, [1 length(kon) * length( bt ) ] );
+end
+end
